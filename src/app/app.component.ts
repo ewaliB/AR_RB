e2d5b20a448bb389f37d8b156c0ea367ab2cc348
ewaliB <esha.wali@tsgforce.com>
2021-02-22 01:19:10 +0530

    Version 2: hardcoing removed from view screens. Menu fixed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rb-proj';
 
   isMinimized:boolean=false;
+  menuOpen:boolean = false;
+
+
+  ngOnInit(){
+    this.resize(null);
+  }
 
   resize(evt:any):void{
-    let width:number = evt.currentTarget.innerWidth;
+    let width:number = 0;
+    if(event)
+      width = evt.currentTarget.innerWidth;
+    else
+      width = window.innerWidth;
     this.isMinimized = ( width <846);
+  }
+
+  minMenuOpen(open:boolean):void{
+      this.menuOpen = open;
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,12 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  pictureData:Array<any> = [
+  {imageLoc:'assets/images/van-gogh.jpg', title:'Starry Night', desc:'This is Starry Night by Van Gogh'},
+  {imageLoc:'assets/images/edwardhopper.jpg', title:'Light House', desc:'Light House by Ed hopper'},
+  {imageLoc:'assets/images/van-gogh2.jpg', title:'Landscape', desc:'Landscape by Van Gogh'},
+  {imageLoc:'assets/images/Sunrise.jpg', title:'Sunrise', desc:'Sunrise by Sunrise'},
+  {imageLoc:'assets/images/portfolio-05.jpg', title:'Fifth Awesome', desc:'Ut sollicitudin risus'},
+  {imageLoc:'assets/images/Caspar_David_Friedrich.jpg', title:'Landscape', desc:'Caspar_David_Friedrich'}];
+
 }
